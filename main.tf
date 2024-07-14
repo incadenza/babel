8eeff65d484e1cb84c245f52e0c4b6908def57ad
incadenza <andrew.dell@melio.com>
2024-07-14 19:22:57 -0400

    try again

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# S3 bucket for static assets
 resource "aws_s3_bucket" "site_bucket" {
   bucket = "remix-site-bucket"
 
@@ -14,7 +15,7 @@ resource "aws_s3_bucket" "site_bucket" {
 resource "aws_s3_bucket_object" "site_files" {
   for_each = fileset("${path.module}/public", "**/*")
 
-  bucket = aws_s3_bucket.site_bucket.bucket
+  bucket = aws_s3_bucket.site_bucket.id
   key    = each.value
   source = "${path.module}/public/${each.value}"
   etag   = filemd5("${path.module}/public/${each.value}")
@@ -23,11 +24,14 @@ resource "aws_s3_bucket_object" "site_files" {
     "html" = "text/html",
     "js"   = "application/javascript",
     "css"  = "text/css"
-  }, fileext("${path.module}/public/${each.value}"), "application/octet-stream")
+  }, fileext(each.value), "application/octet-stream")
 }
 
-resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-  comment = "OAI for S3"
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name   = "s3-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior = "always"
+  signing_protocol = "sigv4"
 }
 
 resource "aws_cloudfront_distribution" "cdn" {
@@ -41,7 +45,7 @@ resource "aws_cloudfront_distribution" "cdn" {
     origin_id   = aws_s3_bucket.site_bucket.id
 
     s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+      origin_access_identity = aws_cloudfront_origin_access_identity.oac.cloudfront_access_identity_path
     }
   }
 
@@ -87,7 +91,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       {
         Effect = "Allow",
         Principal = {
-          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.origin_access_identity.id}"
+          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.oac.id}"
         },
         Action = "s3:GetObject",
         Resource = "arn:aws:s3:::${aws_s3_bucket.site_bucket.id}/*"
@@ -96,93 +100,54 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   })
 }
 
-resource "aws_ecs_cluster" "remix_cluster" {
-  name = "remix-cluster"
-}
-
-resource "aws_ecs_task_definition" "remix_task" {
-  family                   = "remix-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-
-  container_definitions = jsonencode([{
-    name  = "remix-container"
-    image = "your-remix-backend-image" # Replace with your Remix backend Docker image
-    portMappings = [{
-      containerPort = 3000
-      hostPort      = 3000
-      protocol      = "tcp"
-    }]
-    essential = true
-  }])
-}
-
-resource "aws_ecs_service" "remix_service" {
-  name            = "remix-service"
-  cluster         = aws_ecs_cluster.remix_cluster.id
-  task_definition = aws_ecs_task_definition.remix_task.arn
-  desired_count   = 1
-
-  launch_type = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.main.id]
-    security_groups  = [aws_security_group.allow_all.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.remix_tg.arn
-    container_name   = "remix-container"
-    container_port   = 3000
-  }
-}
-
-resource "aws_lb" "remix_lb" {
-  name               = "remix-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow_all.id]
-  subnets            = [aws_subnet.main.id]
-}
-
-resource "aws_lb_target_group" "remix_tg" {
-  name     = "remix-tg"
-  port     = 3000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.remix_lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.remix_tg.arn
-  }
-}
-
-resource "aws_vpc" "main" {
+# VPC
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
+# Subnet
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
 
-resource "aws_security_group" "allow_all" {
-  vpc_id = aws_vpc.main.id
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+# Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Security Group
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -194,10 +159,123 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
+# ECS Cluster
+resource "aws_ecs_cluster" "cluster" {
+  name = "remix-cluster"
+}
+
+# IAM Role for ECS Task
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "task" {
+  family                   = "remix-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "remix-container"
+      image     = "your-remix-backend-image" # Replace with your Docker image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+# ECS Service
+resource "aws_ecs_service" "service" {
+  name            = "remix-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.subnet.id]
+    security_groups  = [aws_security_group.sg.id]
+    assign_public_ip = true
+  }
+}
+
+# ALB
+resource "aws_lb" "lb" {
+  name               = "remix-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg.id]
+  subnets            = [aws_subnet.subnet.id]
+}
+
+# ALB Target Group
+resource "aws_lb_target_group" "tg" {
+  name     = "remix-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+# ALB Listener
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+# ECS Service Load Balancer Attachment
+resource "aws_lb_target_group_attachment" "ecs_attachment" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_ecs_service.service.id
+  port             = 3000
+}
+
 output "cdn_url" {
   value = aws_cloudfront_distribution.cdn.domain_name
 }
 
 output "backend_url" {
-  value = aws_lb.remix_lb.dns_name
+  value = aws_lb.lb.dns_name
 }
